a8efc9b3abefdb04f005886fd895657383504db2
Dhaval Thakkar <dhavalthakkar4995@gmail.com>
2019-06-13 23:58:07 +0530

    Create test-shield.js

---
+++ test-shield.js
@@ -0,0 +1,23 @@
+const pg = require('pg');
+
+pg.defaults.ssl = true;
+
+var config = {
+    connectionString: process.env.DATABASE_URL,
+    max: 4,
+    idleTimeoutMillis: 30000
+}
+
+var pool = new pg.Pool(config)
+
+
+pool.connect(function (err, client, done) {
+  if(err) {
+    process.exit(1);
+  } else {
+    client.query("INSERT INTO test-user VALUES('user1')", function (err, result) {
+      console.log("inserted");
+    });
+  }
+});
+
